e4709691d3068ec48df537ed19f0906db9f5f000
verdetea22 <mikayla.mount22@gmail.com>
2022-09-21 14:12:07 -0400

    everything is still invalid

--- Triangle.py
+++ Triangle.py
@@ -30,13 +30,13 @@ def classifyTriangle(a,b,c):
     # require that the input values be >= 0 and <= 200
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
-        
-    if a <= 0 or b <= b or c <= 0:
+
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
+    if not(isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
         return 'InvalidInput'
   
     # This information was not in the requirements spec but 
@@ -45,19 +45,21 @@ def classifyTriangle(a,b,c):
     # of the specified shape is not a triangle
     if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
- 
+    
+    triangleType = ""
     a, b, c = sorted([a, b, c])    
     # now we know that we have a valid triangle 
     
     if a == b and a == c and b == c:
-        return 'Equilateral'
+        triangleType = 'Equilateral'
         
     elif (((a * a) + (b * b)) == (c * c)) or (((a * a) + (c * c)) == (b * b)) or (((b * b) + (c * c)) == (a * a)):
-        return 'Right'
+        triangleType = 'Right'
         
     elif (a != b) and  (b != c) and (a != c): 
-        return 'Scalene'
+        triangleType = 'Scalene'
         
     else:
-        return 'Isoceles'
+        triangleType = 'Isoceles'
         
+    return triangleType
