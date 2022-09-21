d112a6a1ddecdc43c46bf46128c0b62715b28fd3
verdetea22 <mikayla.mount22@gmail.com>
2022-09-21 11:15:38 -0400

    fixed mistake, Test is in original state

--- TestTriangle.py
+++ TestTriangle.py
@@ -23,6 +23,8 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
     
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
 if __name__ == '__main__':
     print('Running unit tests')
